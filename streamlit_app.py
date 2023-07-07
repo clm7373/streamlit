9d75581b475bc0d17a81df9f8be722e26d38aa57
clm7373 <67660653+clm7373@users.noreply.github.com>
2023-07-07 15:12:30 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,3 @@
 import streamlit as st
-x = st.slider("Select a value")
-st.write(x, "squared is", x * x)
+
 st.write("Here is the pubmed chatbot link [link](https://sightbot.brilliantly.ai?key=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)")
