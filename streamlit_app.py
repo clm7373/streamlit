ea495daa33ec41ca02ba56247abf6c65647001a5
clm7373 <67660653+clm7373@users.noreply.github.com>
2023-07-14 15:24:00 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.write("Here is the pubmed chatbot link [link](https://sightbot.brilliantly.ai?key=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)")
-st.write("Here is the WFSAB chatbot link [link](https://www.filegpt.app/chatbot?collection=clk2tpvug000ykx0fowxhs65j")
+st.write("Here is the WF-SAB chatbot link [link](https://www.filegpt.app/chatbot?collection=clk2tpvug000ykx0fowxhs65j")
