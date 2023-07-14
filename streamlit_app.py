a915fdf168bcd62778fc04418e18f90b0af73766
clm7373 <67660653+clm7373@users.noreply.github.com>
2023-07-14 15:49:44 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-st.write("Here is the pubmed chatbot link [link](https://sightbot.brilliantly.ai?key=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)")
+st.write("Here is the pubmed chatbot link [link](https://sightbot.brilliantly.ai?key=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)")
 st.write("Here is the WF-SAB chatbot link [link](src=https://www.filegpt.app/chatbot?collection=clk0scnx30002mj0fr9ii685r")
